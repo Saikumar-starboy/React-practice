947dd1b6be6aa769cf7c61c1d61c581072e73ec0
Saikumar-starboy <88478772+Saikumar-starboy@users.noreply.github.com>
2023-11-20 13:34:54 +0530

    amount Update

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,18 +1,26 @@
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import Expensedate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails.js"
+import ExpenseDetails from "./ExpenseDetails.js";
 
-function expenseItem(props) {
-  
+const ExpenseItem = (props) => { 
+
+  const [amount , setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setAmount('100');
+    console.log(amount);
+  }
+
   return (
     <div>
       <div  className="expense-item">
        <Expensedate date={props.date}/>
-       <ExpenseDetails title={props.title} amount={props.amount}/>
-        
+       <ExpenseDetails title={props.title} amount={amount}/>
+       <button onClick={clickHandler}>changeAmount</button>
       </div>
     </div>
   );
 }
 
-export default expenseItem;
+export default ExpenseItem;
